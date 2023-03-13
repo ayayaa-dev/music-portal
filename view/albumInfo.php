71a31cd811d8ffa897573ef99d5f47a73065dcba
Exorcist <mtalhead313@gmail.com>
2023-03-13 21:31:33 +0200

    Almost done with design for albumInfo

--- view/albumInfo.php
+++ view/albumInfo.php
@@ -8,58 +8,103 @@ if (isset($album) && $album) {
 
 <body>
     <div class="Full_div">
+
         <div class="Album_div">
-
-            <div class="Album_Picture">
-                <?php
-                echo '<img class="album_cover" src = "' . $album['picture'] . '">';
-                ?>
-            </div>
-
-            <div class="Album_genres">
-                <div class="genre">
-                    <h2>Genre:</h2>
+            <div class="border_album">
+                <div class="Album_Picture">
+                    <?php
+                    echo '<img class="album_img" src = "' . $album['picture'] . '">';
+                    ?>
                 </div>
 
-                <div class="genre_list">
+                <div class="Album_Info">
+                    <div class="Album_Release">
+                        <div class="release">
+                            <h2>Release Date</h2>
+                        </div>
+
+                        <div class="release_Date">
+                            <?php
+                            echo '<p>' . $album['release_date'] . '</p>';
+                            ?>
+                        </div>
+                    </div>
+
+                    <div class="Album_genres">
+                        <div class="genre">
+                            <h2>Genre</h2>
+                        </div>
+
+                        <div class="genre_list">
+                            <?php
+                            echo '<p>' . $album['genre'] . '</p>';
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="Artist_div">
+            <div class="artist_image">
+                <?php
+                $artist = Model::getArtistById($album['artist_id']);
+                echo '<a href="artist?' . $artist['id'] . '">';
+                echo '<img class="artist_pic" src="' . $artist['picture'] . '"></a>'
+                ?>
+            </div>
+            <div class="Artist_Info">
+                <div class="artist_name">
                     <?php
-                    echo '<p>' . $album['genre'] . '</p>';
+                    $artist = Model::getArtistById($album['artist_id']);
+                    echo '<p class = "artist">' . $artist['name'] . '</p>';
+                    ?>
+                </div>
+
+                <div class="artist_short_desc">
+                    <?php
+                    $artist = Model::getArtistById($album['artist_id']);
+                    echo '<p class = "artist_desc">' . $artist['short_desc'] . '</p>';
                     ?>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="Tracks_list">
-            <table>
-                <tr>
-
-                    <th>
-                        <p>Track Name</p>
-                    </th>
-
-                    <th>
-                        <p>Time</p>
-                    </th>
-
-                    <th>
-                        <p>LINK</p>
-                    </th>
-
-                </tr>
-
-                <?php
-                foreach ($tracks as $track) {
-                    echo '<tr>';
-                    echo '<td> <p>' . $track["name"] . '</p>   </td>';
-                    echo '<td> <p>' . $track["time"] . '</p> </td>';
-                    echo '<td><iframe style="border-radius:12px" src="' . $track['link'] . '" width="100%" height="120" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>   </td></td>';
-                    echo '</tr>';
-                }
-
-                ?>
-            </table>
+    <div class="Tracks_list">
+        <hr>
+        <div class="Tracks">
+            <p>Tracks</p>
         </div>
-</div>
+        <table style="width: 100%;">
+            <tr>
+                <th style="width:40%; text-align: center">
+                    <p>Track Name</p>
+                </th>
+
+                <th style="width:25%; text-align: center">
+                    <p>Time</p>
+                </th>
+
+                <th style="width:35%; text-align: center; vertical-align:bottom;">
+                    <p>LINK</p>
+                </th>
+
+            </tr>
+
+            <?php
+            foreach ($tracks as $track) {
+                echo '<tr>';
+                echo '<td> <p style= "text-align:center;">' . $track["name"] . '</p>  </td>';
+                echo '<td> <p style= "text-align:center;">' . $track["time"] . '</p> </td>';
+                echo '<td class="spotify"><iframe style="border-radius:12px;" src="' . $track['link'] . '" width="500px" height="100px" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></td>';
+                echo '</tr>';
+            }
+
+            ?>
+        </table>
+    </div>
 </body>
 
 <?php
