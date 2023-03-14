34452b191b51feb3028dff81793f4a7127994b8b
Exorcist <90632640+HeIIBlazer@users.noreply.github.com>
2023-03-14 23:25:23 +0200

    Re-made artistList Page

--- view/artistsList.php
+++ view/artistsList.php
@@ -23,19 +23,6 @@ $title = "Artists"
         }
         ?>
     </div>
-
-    <!-- old design -->
-    <!-- <?php
-            foreach ($artistList as $artist) {
-                echo '<div style="display:flex;flex-direction: column;text-align:center;width:25%;margin-bottom:16px;">';
-                echo '<a href="artist?' . $artist['id'] . '" style="font-size: 18px;text-decoration: none; color: black;">';
-                echo '<div style="display: flex; justify-content: center;">';
-                echo '<img src="' . $artist['picture'] . '" style="width:250px; height: 250px">';
-                echo '</div>';
-                echo '<a href="artist?' . $artist['id'] . '" style="font-size: 18px;text-decoration: none; color: black;">' . $artist['name'] . '</a>';
-                echo '</div></a>';
-            }
-            ?> -->
 </div>
 <?php
 $content = ob_get_clean();
